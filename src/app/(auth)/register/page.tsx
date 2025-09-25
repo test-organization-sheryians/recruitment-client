88f88e965307ca7b1383e10b504a0669b2e1eaa1
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-09-25 18:48:37 +0530

    created register page UI

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import SignupForm from '@/features/auth/components/SignupForm'
 
 const page = () => {
   return (
-    <div>
-
+    <div className="h-screen w-full bg-[#DCDCDC] overflow-hidden flex gap-3.5 p-5 font-[satoshi]">
+      <div className="bg-white w-1/3 rounded-base overflow-hidden">
+        <SignupForm />
+      </div>
+      <div className=" w-2/3 rounded-base bg-[url('/images/signimg2.webp')] bg-center bg-cover overflow-hidden"></div>
     </div>
   )
 }
---
+++ src/features/auth/components/SignupForm.tsx
@@ -0,0 +1,68 @@
+import { CiMail } from "react-icons/ci";
+import { FcGoogle } from "react-icons/fc";
+import LabelInput from "./LabelInput";
+
+const SignupForm = () => {
+  return (
+    <div className="w-full h-full font-[satoshi] bg-white rounded-2xl py-10 px-[20%] flex flex-col justify-center">
+      <h1 className="text-3xl font-semibold text-center text-gray-800">
+        Sign-up Account
+      </h1>
+
+      <form className="mt-8 space-y-5">
+        {/* Email */}
+        <LabelInput
+          label="Email"
+          placeholder="your email"
+          type="email"
+          id="email"
+        />
+
+        {/* Password */}
+        <LabelInput
+          label="Password"
+          placeholder="8+ characters"
+          type="password"
+          id="password"
+        />
+
+        <LabelInput
+          label="Confirm Password"
+          placeholder="8+ characters"
+          type="password"
+          id="password"
+        />
+
+        {/* Continue Button */}
+        <button className="w-full bg-[#4C62ED] hover:bg-[#3a4cd1] transition-colors text-white text-base font-medium rounded-base py-2.5 capitalize flex items-center justify-center gap-2 cursor-pointer">
+          <CiMail className="text-lg" /> Continue with Email
+        </button>
+
+        {/* Divider */}
+        <div className="flex items-center justify-center my-4">
+          <span className="flex-1 border-t border-gray-300"></span>
+          <span className="mx-3 text-gray-400 text-xs font-medium">OR</span>
+          <span className="flex-1 border-t border-gray-300"></span>
+        </div>
+
+        {/* Google Button */}
+        <button className="w-full bg-[#3B3A3A] hover:bg-black transition-colors text-white text-base font-medium rounded-base py-2.5 capitalize flex items-center justify-center gap-2 cursor-pointer">
+          <FcGoogle className="text-lg" /> Continue with Google
+        </button>
+      </form>
+
+      {/* Register */}
+      <p className="text-center text-gray-600 text-sm mt-6">
+        Already a user?{" "}
+        <a
+          href="/login"
+          className="text-[#4C62ED] underline font-medium"
+        >
+          Sign In
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default SignupForm;
