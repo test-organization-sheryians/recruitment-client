2bdd9b4de77c9c6967c688a3474608674eeca96a
sarang <sarangkale66@gmail.com>
2025-09-25 18:14:34 +0530

    label input component created

--- src/features/auth/components/SigninForm.tsx
+++ src/features/auth/components/SigninForm.tsx
@@ -1,5 +1,6 @@
 import { CiMail } from "react-icons/ci";
 import { FcGoogle } from "react-icons/fc";
+import LabelInput from "./LabelInput";
 
 const SigninForm = () => {
   return (
@@ -10,36 +11,20 @@ const SigninForm = () => {
 
       <form className="mt-8 space-y-5">
         {/* Email */}
-        <div className="flex flex-col">
-          <label
-            htmlFor="email"
-            className="text-gray-600 text-sm font-medium mb-1"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="your email"
-            className="w-full text-base bg-[#DFECFF] rounded-base px-4 py-2.5 outline-none"
-          />
-        </div>
+        <LabelInput
+          label="Email"
+          placeholder="your email"
+          type="email"
+          id="email"
+        />
 
         {/* Password */}
-        <div className="flex flex-col">
-          <label
-            htmlFor="password"
-            className="text-gray-600 text-sm font-medium mb-1"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="8+ characters"
-            className="w-full text-base bg-[#DFECFF] rounded-base px-4 py-2.5 outline-none"
-          />
-        </div>
+        <LabelInput
+          label="Password"
+          placeholder="8+ characters"
+          type="password"
+          id="email"
+        />
 
         {/* Continue Button */}
         <button className="w-full bg-[#4C62ED] hover:bg-[#3a4cd1] transition-colors text-white text-base font-medium rounded-base py-2.5 capitalize flex items-center justify-center gap-2 cursor-pointer">
