cd5ad847e98de6117f87a937150e720215c413b3
Dipu <dipwagh2345@gmail.com>
2025-09-25 17:57:23 +0530

    feat(ui): refine sign-in page layout and styling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import SigninForm from '@/features/auth/SigninForm'
 
 const page = () => {
   return (
-    <div>
-      login
+    <div className="h-screen w-full bg-[#DCDCDC] overflow-hidden flex gap-3.5 p-5 font-[satoshi]">
+        <div className="bg-white w-1/3 rounded-base overflow-hidden">
+            <SigninForm />
+        </div>
+        <div className=" w-2/3 rounded-base bg-[url('/images/signimg1.webp')] bg-center bg-cover overflow-hidden"></div>
     </div>
   )
 }
