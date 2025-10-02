fec1a8528933b7746eb09ca640f027459f52a61d
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-10-02 15:53:20 +0530

    feat: integrate api and response interceptor

--- src/features/auth/components/LabelInput.tsx
+++ src/features/auth/components/LabelInput.tsx
@@ -1,34 +1,42 @@
 import React from 'react'
 
-interface LabelInputIntern {
+interface LabelInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   type: string;
-  id: string;
+  id?: string;
   placeholder: string;
+  name?: string;
 }
 
-const LabelInput = ({
+const LabelInput = React.forwardRef<HTMLInputElement, LabelInputProps>(({
   label,
   type,
   id,
-  placeholder
-}: LabelInputIntern) => {
+  placeholder,
+  ...rest
+}, ref) => {
+  const _id = id || React.useId()
+
   return (
     <div className="flex flex-col">
       <label
-        htmlFor="email"
+        htmlFor={_id}
         className="text-gray-600 text-sm font-medium mb-1"
       >
         {label}
       </label>
       <input
         type={type}
-        id={id}
+        id={_id}
         placeholder={placeholder}
         className="w-full text-base bg-[#DFECFF] rounded-base px-4 py-2.5 outline-none"
+        ref={ref}
+        {...rest}
       />
     </div>
   )
-}
+})
 
-export default LabelInput
+LabelInput.displayName = 'LabelInput';
+
+export default LabelInput;
