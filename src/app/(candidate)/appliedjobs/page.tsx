5eb490a845e39c5836e026ebc9b57181f062ce3f
Anshu Pandey <anshu@Anshus-MacBook-Air.local>
2025-12-23 18:27:17 +0530

    Revert "Merge pull request #60 from test-organization-sheryians/appliedjobs"
    This reverts commit 6ccb31b53cd5a2dbfd6dc82f4b20602eff2249fd, reversing
    changes made to b0fc204e4a0fc8af7243d6ab87896440717ae27d.

--- src/app/(candidate)/appliedjobs/page.tsx
+++
@@ -1,5 +0,0 @@
-import AppliedJobs from "@/features/applied-jobs/component/AppliedJobs";
-
-export default function AppliedJobsPage() {
-  return <AppliedJobs />;
-}
--- src/features/applied-jobs/services/appliedJobs.api.ts
+++
@@ -1,7 +0,0 @@
-import apiClient from "@/lib/api-client";
-import { AppliedJob } from "../types/appliedjobs.types";
-
-export async function getAppliedJobs(): Promise<AppliedJob[]> {
-  const res = await apiClient.get("/job-apply/my-applications");
-  return res.data.data;
-}
