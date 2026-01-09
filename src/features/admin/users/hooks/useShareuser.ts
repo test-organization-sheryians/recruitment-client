d1960efb1c31b7d1fa2e74c4afec28ea3c40abf5
sabhi-manu <sabhimanu707@gmail.com>
2026-01-09 21:48:28 +0530

    hook and api

---
+++ src/features/admin/users/hooks/useShareuser.ts
@@ -0,0 +1,42 @@
+import { createShareCandidate, getShareCandidate } from "@/api/candidateShare/shareCandidate";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+interface ShareCandidatePayload {
+  candidateId: string;
+}
+
+interface BackendResponse<T> {
+  success: boolean;
+  message?: string;
+  data: T[];
+}
+
+interface ShareCandidate {
+  _id: string;
+  candidateId: string;
+  email?: string;
+  name?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export const useCreateShareCandidate = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (payload: ShareCandidatePayload[]) =>createShareCandidate(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["share-candidates"],
+      });
+    },
+  });
+};
+
+
+export const useShareCandidates = () => {
+  return useQuery<BackendResponse<ShareCandidate>>({
+    queryKey: ["share-candidates"],
+    queryFn: getShareCandidate,
+  });
+};
