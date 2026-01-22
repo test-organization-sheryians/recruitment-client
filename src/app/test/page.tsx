fade2a628ac81dd6e2d2cdde8103c50d39e1df91
Anshu Pandey <anshu@Anshus-MacBook-Air.local>
2026-01-22 16:13:55 +0530

    deleted test route

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import FileUploadComponent from "@/features/admin/test/components/FileUploadComponent";
-
-export default function Page() {
-  return (
-    <FileUploadComponent apiEndpoint="/api/upload" />
-  );
-}
