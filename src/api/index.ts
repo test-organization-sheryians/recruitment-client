9b2902fa8da4db0725206a45e86bfe94085b3bcb
AnshikaXhack <148768843+AnshikaXhack@users.noreply.github.com>
2026-01-31 16:20:36 +0530

    first commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -73,3 +73,8 @@ export * from "./password/resetPassword";
 
 export * from "./jobApplication/scheduleInterview";
 
+// categoriess
+ export * from "./categoriess/addCategoriess"
+ export * from "./categoriess/getCategoriess"
+ export * from "./categoriess/updateCategoriess"
+ export * from "./categoriess/deleteCategoriess"
