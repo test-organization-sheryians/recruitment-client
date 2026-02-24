0f02cef390b93d5b98585cb4bf732900f35ae373
ayushudaydubey <102001ayushdubey@gmail.com>
2026-02-24 16:20:49 +0530

    CRUD product

--- src/lib/product-api.ts
+++ src/lib/product-api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 //  using axios 
+// for product API
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:9000/api",
@@ -26,5 +27,6 @@ apiClient.interceptors.response.use(
     }
 
     return Promise.reject(error);
+    
   }
 );
